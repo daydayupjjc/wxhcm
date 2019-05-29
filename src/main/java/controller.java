6391e73ceffd955688431839a204f9087b3dd193
jjc <fashstop@163.com>
2019-05-30 01:15:41 +0800

    first commit

---
+++ src/main/java/controller.java
@@ -0,0 +1,11 @@
+
+public class controller {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("hh");
+
+    }
+
+}
---
+++ src/main/java/controller1.java
@@ -0,0 +1,11 @@
+
+public class controller1 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("hh");
+
+    }
+
+}
